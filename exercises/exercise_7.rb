19d6366b84eca47b5c031452174256104a07f0fa
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-04-11 16:19:12 -0700

    Add exercise 7 code

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+end
+
+print "What is a store name?"
+name = $stdin.gets.chomp
+
+new_store = Store.create(name: name)
+new_store.errors.each {|error| puts error.message}
